f027d22716b5cdb993c0e99f25ac997cf5d44529
vo1opas <volnaegoizma@gmail.com>
2022-07-22 18:42:33 +0300

    dz_4

---
+++ zd_1/Program.cs
@@ -0,0 +1,41 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+int i;
+bool isPalindrom = true;
+string str;
+
+
+Console.WriteLine("Введите строку:");
+str = Console.ReadLine();
+
+if (str.Length % 2 != 0)
+{
+    for (i = 0; i < str.Length / 2; i++)
+    {
+        if (str[i] != str[str.Length - 1 - i])
+        {
+            isPalindrom = false;
+            break;
+        }
+    }
+
+}
+else
+{
+    isPalindrom = false;
+}
+
+if (isPalindrom)
+{
+    Console.WriteLine("Палиндром");
+}
+else
+{
+    Console.WriteLine("Не палиндром");
+}
---
+++ zd_3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите N:");
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= count; i++)
+{
+    Console.WriteLine(powCube(i));
+}
+int powCube(int number)
+{
+    return number * number * number;
+}
